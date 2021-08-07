977520a6317e9353beba490883de8f1bace04d98
vivi <vivi@precision.broadband>
2021-08-07 11:43:19 +0100

    Records to timecamp

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+import requests
+from datetime import date
+
+
+def record(start, end, description):
+    today = date.today()
+    url = "https://app.timecamp.com/third_party/api/entries"
+
+    payload = {'date': today.strftime("%Y-%m-%d"),
+               'start': start,
+               'end': end,
+               'note': 'email note',
+               'description': description
+               }
+
+    #payload = "{\"date\":\"2021-03-02\",\"start_time\":\"2020-02-02 14:05:47\",\"end\":\"2020-02-02 16:21:31\",\"note\":\"string\",\"description\":\"string\"}"
+
+    headers = {
+        'authorization': "cd9eab8a7b4551e664c2e0a9ed",
+        'Content-Type': "application/json"
+    }
+
+    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+
+    print(response.text)
+
+
+def tia():
+    print("starting tia...")
+    record(start='19:05:47', end='20:21:31', description='email description')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    tia()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
