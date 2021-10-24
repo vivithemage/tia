c96f5f8c1e2ddd9f3ee3d7f83460d9a891c37e5a
vivi <rs@mage.me.uk>
2021-10-24 23:18:00 +0100

    Recording properly now

--- scratch/press_and_record_demo.py
+++ scratch/press_and_record_demo.py
@@ -33,17 +33,18 @@ class VerbalLog:
         t.start()
 
     def stop_recording(self):
+        self.is_recording = False
+        self.filename = "recording" + str(random.randint(1, 100)) + ".wav"
+        print('recording complete')
+
         try:
-            self.is_recording = False
-            self.filename = "recording" + str(random.randint(1, 100)) + ".wav"
-            print('recording complete')
-            # wf = wave.open(self.filename, 'wb')
-            # wf.setnchannels(self.channels)
-            # wf.setsampwidth(self.p.get_sample_size(self.sample_format))
-            # wf.setframerate(self.fs)
-            # wf.writeframes(b''.join(self.frames))
-            # wf.close()
-            #
+            wf = wave.open(self.filename, 'wb')
+            wf.setnchannels(self.channels)
+            wf.setsampwidth(self.p.get_sample_size(self.sample_format))
+            wf.setframerate(self.fs)
+            wf.writeframes(b''.join(self.frames))
+            wf.close()
+
             self.frames = []
             # self.stream.close()
 
@@ -65,19 +66,19 @@ class Tia:
     def rising(self, channel):
         print("released")
         gpio.remove_event_detect(3)
-        gpio.add_event_detect(3, gpio.FALLING, callback=self.falling, bouncetime=5)
+        gpio.add_event_detect(3, gpio.FALLING, callback=self.falling, bouncetime=100)
         self.recorder.stop_recording()
 
     def falling(self, channel):
         print("pressed")
         gpio.remove_event_detect(3)
-        gpio.add_event_detect(3, gpio.RISING, callback=self.rising, bouncetime=5)
+        gpio.add_event_detect(3, gpio.RISING, callback=self.rising, bouncetime=100)
         self.recorder.start_recording()
 
     def run(self):
         print("starting tia...")
         gpio.setup(3, gpio.IN, pull_up_down=gpio.PUD_UP)
-        gpio.add_event_detect(3, gpio.FALLING, callback=self.falling, bouncetime=5)
+        gpio.add_event_detect(3, gpio.FALLING, callback=self.falling, bouncetime=100)
 
         message = input("Press enter to quit\n\n")  # Run until someone presses enter
 
